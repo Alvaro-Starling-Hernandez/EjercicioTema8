94080c877487ab120a7d482dfad9ce6a150e548a
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-11-30 14:59:46 -0400

    Adding person class and printing its properties

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona person = new Persona();
+        person.setAge(20);
+        person.setName("Alvaro");
+        person.setPhone("829-817-4805");
+        System.out.println("Age: "+person.getAge());
+        System.out.println("Name: "+person.getName());
+        System.out.println("Phone: "+person.getPhone());
     }
 }
---
+++ src/Persona.java
@@ -0,0 +1,29 @@
+public class Persona {
+    private int age;
+    private String name;
+    private String phone;
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
